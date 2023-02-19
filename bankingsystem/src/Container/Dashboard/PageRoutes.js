3cfb68ad93d73b0ddc498a300a2c6487aeaffe25
Tsigab <107885237+Tsigab1996@users.noreply.github.com>
2023-02-18 22:32:11 -0600

    modified again for the  same implementations

--- bankingsystem/src/Container/Dashboard/PageRoutes.js
+++ bankingsystem/src/Container/Dashboard/PageRoutes.js
@@ -4,49 +4,8 @@ export default function PageRoutes() {
     return (
         <div>
             <Routes>
-                {/* <Route path="/">
-                    <Route index element={<Properties />} />
-                    <Route path=":id" element={<PropertyDetails />}></Route>
-                </Route>
 
-                <Route path="rent">
-                    <Route index element={<Rent />} />
-                    <Route path=":id" element={<PropertyDetails />}></Route>
-                </Route>
-
-                <Route path="buy">
-                    <Route index element={<Buy />} />
-                    <Route path=":id" element={<PropertyDetails />}></Route>
-                </Route>
-
-                <Route path="favorites">
-                    <Route index element={<FavoriteList />} />
-                    <Route path=":id" element={<PropertyDetails />}></Route>
-                </Route>
-
-                <Route path="rentedproperties" element={<RentedProperties />}></Route>
-                <Route path="recentcustomers" element={<RecentCustomer />}></Route> */}
-
-                
-                {/* Admin */} 
-
-                 <Route path="viewemployee" element={<Customers />}></Route>
-                <Route path="registeremployee" element={<Owner />}></Route>
-
-                {/* Banker */}
-
-                <Route path="addcustomer" element={<MyProperties />}></Route>
-                <Route path="viewcustomerinfo" element={<Applications />}></Route>
-                <Route path="managecustomer" element={<AddProperty />}></Route>
-
-                {/* Customer */}
-                <Route path="viewbalance" element={<RentedProperties />}></Route>
-                <Route path="deposit" element={<RecentCustomer />}></Route>
-                <Route path="withdraw" element={<RecentCustomer />}></Route>
-                <Route path="viewtransaction" element={<RecentCustomer />}></Route>
-
-                 {/* log in */}
-                 <Route path="login" element={<LogInPage />}></Route>
+              
                 
             </Routes>
         </div>
